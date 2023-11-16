5833b02a0d6c014816c80431502e9241b953ba07
TrueKing <elitrueking@gmail.com>
2023-11-16 16:19:41 +0000

    File naming made dynamic

--- lib/to_csv.dart
+++ lib/to_csv.dart
@@ -1,18 +1,17 @@
 library to_csv;
 
 import 'dart:convert';
- 
+
 import 'package:file_saver/file_saver.dart';
 import 'package:flutter/foundation.dart';
 import 'package:csv/csv.dart';
 import 'package:intl/intl.dart';
 import 'package:share_plus/share_plus.dart';
 import 'dart:io';
-import 'package:universal_html/html.dart' as html; 
+import 'package:universal_html/html.dart' as html;
 
-
-
-myCSV(List<String> headerRow, List<List<String>> listOfListOfStrings,{bool sharing = false}) async{
+myCSV(List<String> headerRow, List<List<String>> listOfListOfStrings,
+    {bool sharing = false, String? fileName, String? fileTimeStamp}) async {
   debugPrint("***** Gonna Create cv");
 
   //* A list of header
@@ -34,6 +33,9 @@ myCSV(List<String> headerRow, List<List<String>> listOfListOfStrings,{bool shari
   //   //Now that its confirmed that length of header elements and row elemnts are same lets create the csvFile
   // }
 
+  //specify file name
+  String givenFileName = "${fileName ?? 'item_export'}_";
+
   //create the final list of lists containing the header and the data
   List<List<String>> headerAndDataList = [];
   headerAndDataList.add(headerRow);
@@ -44,26 +46,26 @@ myCSV(List<String> headerRow, List<List<String>> listOfListOfStrings,{bool shari
   String csvData = const ListToCsvConverter().convert(headerAndDataList);
 
   DateTime now = DateTime.now();
+
+  //allow user to specify a specific timestamp that was saved with record
   String formattedData =
-  DateFormat('MM-dd-yyyy-HH-mm-ss')
-      .format(now);
+      fileTimeStamp ?? DateFormat('MM-dd-yyyy-HH-mm-ss').format(now);
+
   if (kIsWeb) {
     final bytes = utf8.encode(csvData);
     final blob = html.Blob([bytes]);
-    final url =
-    html.Url.createObjectUrlFromBlob(blob);
-    final anchor = html.document
-        .createElement('a') as html.AnchorElement
+    final url = html.Url.createObjectUrlFromBlob(blob);
+    final anchor = html.document.createElement('a') as html.AnchorElement
       ..href = url
       ..style.display = 'none'
-      ..download =
-          'item_export_$formattedData.csv';
+      ..download = '$givenFileName$formattedData.csv';
     html.document.body!.children.add(anchor);
     anchor.click();
     html.Url.revokeObjectUrl(url);
-  }
-  else if(Platform.isAndroid || Platform.isIOS || Platform.isWindows || Platform.isMacOS) {
-  
+  } else if (Platform.isAndroid ||
+      Platform.isIOS ||
+      Platform.isWindows ||
+      Platform.isMacOS) {
 /*    Directory? director = await getExternalStorageDirectory();
     debugPrint('2');
     final File file = await (File('${director!.path}/item_export_$formattedData.csv').create());
@@ -73,11 +75,15 @@ myCSV(List<String> headerRow, List<List<String>> listOfListOfStrings,{bool shari
     final bytes = utf8.encode(csvData);
     Uint8List bytes2 = Uint8List.fromList(bytes);
     MimeType type = MimeType.csv;
-    String? unknownValue = await FileSaver.instance.saveAs(name:'item_export_$formattedData.csv',bytes: bytes2, ext:'csv',mimeType:type);
+    String? unknownValue = await FileSaver.instance.saveAs(
+        name: '$givenFileName$formattedData.csv',
+        bytes: bytes2,
+        ext: 'csv',
+        mimeType: type);
     print("Unknown value $unknownValue");
-    if(sharing == true){ 
-        XFile xFile = XFile.fromData(bytes2); 
-        await Share.shareXFiles([xFile], text: 'Csv File');
+    if (sharing == true) {
+      XFile xFile = XFile.fromData(bytes2);
+      await Share.shareXFiles([xFile], text: 'Csv File');
     }
   }
 }
