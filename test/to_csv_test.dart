35cbf1a2139e27e6d5a5415ed82b82c1e5d5c6d3
Thomas Sanderson <Thomas.Sanderson@hansencx.com>
2024-06-21 11:46:25 -0400

    issue-15: intl & flutter analyze fixes
    Update to bump intl to ^0.19.0 to work with the latest flutter release, and fixed issues identified by flutter analyze.

--- test/to_csv_test.dart
+++ test/to_csv_test.dart
@@ -1,8 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:to_csv/to_csv.dart';
-
 void main() {
-  test('adds one to input values', () {
-  });
+  test('adds one to input values', () {});
 }
